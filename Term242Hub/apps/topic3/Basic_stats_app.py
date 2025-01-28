a201a2fb224184e42ee55bbb0ab174b68de29015
drmohammadnabhan <drmohammadnabhan@gmail.com>
2025-01-28 21:38:16 +0300

    Update Basic_stats_app.py

--- Term242Hub/apps/topic3/Basic_stats_app.py
+++ Term242Hub/apps/topic3/Basic_stats_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title="Topic 1 - App 1", page_icon="📊")
-st.title("Topic 1 - App 1")
+st.title("Topic 3 - App 1")
 st.write("This is the first app under Topic 1.")
